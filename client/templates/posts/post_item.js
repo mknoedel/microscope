5ecf196538efaf99595af7d57279fb2456bcccf0
Michael Knoedel <mknoedel@clearviewaudit.com>
2017-08-25 10:21:43 -0500

    Added edit posts form.

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,6 +1,9 @@
 Template.postItem.helpers({
+    ownPost: function () {
+        return this.userId === Meteor.userId();
+    },
     domain: function() {
-        var a = document.createElement('a');
+        let a = document.createElement('a');
         a.href = this.url;
         return a.hostname;
     }
