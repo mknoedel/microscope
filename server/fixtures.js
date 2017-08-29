65b26bde9f1dd371a1aa4eda88e9a12297e005f8
mknoedel <mknoedel@clearviewaudit.com>
2017-08-29 16:21:26 -0500

    Added enough posts that pagination is necessary.

--- server/fixtures.js
+++ server/fixtures.js
@@ -54,4 +54,15 @@ if (Posts.find().count() === 0) {
         submitted: new Date(now - 12 * 3600 * 1000),
         commentsCount: 0
     });
+
+    for (let i=0; i < 10; i++) {
+        Posts.insert({
+            title: 'Test post #' + i,
+            author: sacha.profile.name,
+            userId: sacha._id,
+            url: 'http://google.com/?q=test-' + i,
+            submitted: new Date(now - i * 36000 * 1000),
+            commentsCount: 0
+        });
+    }
 }
