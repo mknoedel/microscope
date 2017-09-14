f747f36bd0ecb5f032fb1245172ada72db2514b1
mknoedel <mknoedel@clearviewaudit.com>
2017-09-14 17:08:56 -0500

    Added Intercom code snippit.

---
+++ client/main.js
@@ -0,0 +1,7 @@
+window.intercomSettings = {
+    app_id: "tded6jmr",
+    email: "mknoedel@clearviewaudit.com",
+    user_id: "34563456",
+    created_at: 1505426798, // Current UNIX timestamp,
+    name: 'Michael Developer'
+};
