8bf2d4d3aa9f13f2d45feec5e88167cff90ad59f
mknoedel <mknoedel@clearviewaudit.com>
2017-09-14 17:21:49 -0500

    Sending custom user data.

--- client/main.js
+++ client/main.js
@@ -1,7 +1,12 @@
-window.intercomSettings = {
-    app_id: "tded6jmr",
-    email: "mknoedel@clearviewaudit.com",
-    user_id: "34563456",
-    created_at: 1505426798, // Current UNIX timestamp,
-    name: 'Michael Developer'
-};
+Tracker.autorun(function () {
+    if (Meteor.user() && !Meteor.loggingIn()) {
+        let intercomSettings = {
+            name: Meteor.user().username,
+            email: "mknoedel@clearviewaudit.com",
+            created_at: Math.round(Meteor.user().createdAt / 1000), // Current UNIX timestamp,
+            favorite_color: _.sample(['blue', 'red', 'green', 'yellow']),
+            app_id: "tded6jmr"
+        };
+        Intercom('boot', intercomSettings);
+    }
+});
